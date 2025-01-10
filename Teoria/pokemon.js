bd76ab304db8967d0531b95c8de094395c6d2cd6
Lucas <lucasge676@gmail.com>
2025-01-09 21:47:28 -0300

    Eliminé la Carpeta Teoria

--- Teoria/pokemon.js
+++
@@ -1,8 +0,0 @@
-const txt = document.getElementById("respuesta")
-async function obtenerDatos() {
-    await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error=>console.log('Solicitud Fallida', error))
-}
-obtenerDatos();
