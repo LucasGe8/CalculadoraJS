7d58c0086a2281bfcb8924559e37131840513e93
Lucas <lucasge676@gmail.com>
2025-01-10 21:38:37 -0300

    Calculator Acces By Menu Added and Operational Signs fixed

--- Menu1/script.js
+++ Menu1/script.js
@@ -11,5 +11,23 @@ function activarLink(){
     item.classList.remove('active'));
     this.classList.add('active');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleccionamos el enlace con la clase 'link' dentro de 'list'
+    const linkElement = document.querySelector('.list.link a'); 
+    if (linkElement && linkElement.getAttribute('href')) { // Aseguramos que el enlace exista en el DOM
+        linkElement.addEventListener('click', function (event) {
+        event.preventDefault(); // Detener la redirección inmediata
+        const link = linkElement.getAttribute('href'); // Obtener el valor del atributo href del enlace
+        console.log("Enlace de redirección: ", link); // Para depuración
+        setTimeout(() => {
+          window.location.href = link; // Realizamos la redirección después de que haya pasado el tiempo de la animación
+        }, 500); // Tiempo igual al de la animación (0.3s)
+      });
+    } else {
+      console.error('El enlace no se encontró'); // Si no se encuentra el enlace
+    }
+  });
+
 list.forEach((item)=>
 item.addEventListener('click',activarLink));
